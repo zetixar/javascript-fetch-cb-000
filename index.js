bbbaab9399be5e613d10341bbeb181ae223327e3
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-30 17:22:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
